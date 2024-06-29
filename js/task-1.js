b876bd024348e366dca647b7544847709e913aaf
Valeriia Zubrytska <valeriia.zubrytska@gmail.com>
2024-06-29 12:04:28 +0300

    change-t1-t3-t6

--- js/task-1.js
+++ js/task-1.js
@@ -1,16 +1,12 @@
 console.group('Завдання 1')
-const ul = document.getElementById('categories');
+
 const items = document.querySelectorAll('.item');
-const titles = document.querySelectorAll('h2');
-
-
 
 console.log("Numbers of categories:", items.length)
 
 items.forEach((item) => {
-    const child = item.children;
-    console.log('Category:', child[0].textContent);
-    console.log('Elements:', child[1].children.length);
+    console.log('Category:', item.firstElementChild.textContent);
+    console.log('Elements:', item.lastElementChild.children.length);
 })
 
 console.groupEnd();
--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,7 @@
 const input = document.getElementById('name-input');
 const inputName = document.getElementById('name-output');
 
-input.addEventListener('change', () => {
+input.addEventListener('input', () => {
     const valueWithoutSpaces = input.value.trim();
     inputName.textContent = valueWithoutSpaces;
     if (!valueWithoutSpaces) {
